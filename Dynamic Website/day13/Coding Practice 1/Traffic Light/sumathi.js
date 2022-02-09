a4ac16404922b22712712cc2b9e54b84561271a2
sumathi3399 <sumathivarma3399@gmail.com>
2022-02-09 23:05:56 +0530

    Traffic Light Practice Done

---
+++ Dynamic Website/day13/Coding Practice 1/Traffic Light/sumathi.js
@@ -0,0 +1,28 @@
+function buttonStop(){
+    document.getElementById("buttonStop").style.backgroundColor = "red";
+    document.getElementById("buttonStopB").style.backgroundColor = "red";
+    document.getElementById("buttonReadyB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonReady").style.backgroundColor = "#1f1d41";
+    document.getElementById("buttonGoB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonGo").style.backgroundColor = "#1f1d41";
+
+}
+function buttonReady(){
+    document.getElementById("buttonReady").style.backgroundColor = "yellow";
+    document.getElementById("buttonReadyB").style.backgroundColor = "yellow";
+    document.getElementById("buttonGoB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonGo").style.backgroundColor = "#1f1d41";
+    document.getElementById("buttonStopB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonStop").style.backgroundColor = "#1f1d41";
+
+
+}
+function buttonGo(){
+    document.getElementById("buttonGo").style.backgroundColor = "green";
+    document.getElementById("buttonGoB").style.backgroundColor = "green";
+    document.getElementById("buttonReadyB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonReady").style.backgroundColor = "#1f1d41";
+    document.getElementById("buttonStopB").style.backgroundColor = "#4b5069";
+    document.getElementById("buttonStop").style.backgroundColor = "#1f1d41";
+
+}

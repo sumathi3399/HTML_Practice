21dea0d9a5fc5ff31a8e08c002139ee20b1de246
sumathi3399 <sumathivarma3399@gmail.com>
2022-02-20 17:39:06 +0530

    JS Coding Practice 1 done

---
+++ Dynamic Website/day24/JS Coding Practice 1/sumathi.js
@@ -0,0 +1,31 @@
+// create & log an array
+let output = ['orange', 25,100,true,33.58];
+console.log(output);
+
+// access the values of an array
+let myArray = [25,0,78,23,35];
+console.log(myArray[0]);
+
+// updating the values of an array
+let myArray1 = [7,50,'center',75,100];
+let val =5;
+myArray1[0]=val;
+console.log(myArray1);
+
+// find the length of an array
+let arrayLength = [12,1,2,4,1];
+console.log(arrayLength.length);
+
+// add value to the end of the array
+let myArray2 = [1,2,4];
+let val1 = 2;
+myArray2.push(val1);
+console.log(myArray2);
+
+// delete the last value of an array
+let myArray3 = [17,2,4,9];
+myArray3.pop();
+console.log(myArray3);
+
+
+
